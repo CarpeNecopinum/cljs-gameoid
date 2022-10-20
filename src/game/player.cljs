f86cce61549066b866b674adea38701c92593d7c
Christian Mattes <CarpeNecopinum@noreply.users.github.com>
2022-10-20 19:10:11 +0200

    Allow diagonal movement of the player

--- src/game/player.cljs
+++ src/game/player.cljs
@@ -8,25 +8,28 @@
 (defonce anim_speed 0.1)
 (defonce run_speed 2)
 
-;;; update the :dir component based on the pressed keys
-;;; count up the :frame component when a direction key is pressed
-;;; reset the :frame component when it reaches the end of the sprite sheet
-
 (defn update-player [player]
-  (let [{:keys [frame x y] :or {frame 0}} player
+  (let [{:keys [frame x y dir] :or {frame 0, dir :down}} player
+        xdir (cond (input/key-pressed? 37) :left
+                   (input/key-pressed? 39) :right
+                   :else :none)
+        ydir (cond (input/key-pressed? 38) :up
+                   (input/key-pressed? 40) :down
+                   :else :none)
         [dir running] (cond
-                        (input/key-pressed? 37) [:left true]
-                        (input/key-pressed? 39) [:right true]
-                        (input/key-pressed? 38) [:up true]
-                        (input/key-pressed? 40) [:down true]
-                        :else [(get player :dir :down) false])
+                        (not= ydir :none) [ydir true]
+                        (not= xdir :none) [xdir true]
+                        :else [dir false])
         frame (if running (mod (+ frame anim_speed) run_frames) 0)
-        [x y] (if running (case dir
-                            :left [(- x run_speed) y]
-                            :right [(+ x run_speed) y]
-                            :up [x (- y run_speed)]
-                            :down [x (+ y run_speed)])
-                  [x y])
+        run_speed (if (and (not= xdir :none) (not= ydir :none))
+                    (/ run_speed 1.414)
+                    run_speed)
+        x (cond (= xdir :left) (- x run_speed)
+                (= xdir :right) (+ x run_speed)
+                :else x)
+        y (cond (= ydir :up) (- y run_speed)
+                (= ydir :down) (+ y run_speed)
+                :else y)
         iframe (int frame)
         sprite-col (case dir :right 0 :up 6 :left 12 :down 18)
         sprite {:image alex_run
